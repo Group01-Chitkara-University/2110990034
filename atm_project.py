375bc0d0f3026b87b6fa4e21011d2a66aa5e7aee
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-20 10:37:16 +0530

    deposit function and withdraw function added

--- atm_project.py
+++ atm_project.py
@@ -68,6 +68,48 @@ def printMainMenu(username):
         print("\nThank you, ",username, "! Program exiting ......")
         sleep(1)
         start()
+
+'''
+    Deposit function: 
+        1. asks the user for amount to be entered and then increments the balance at the same index that the username has to avoid redundancy
+        2. after that displays the message of transaaction successful
+        3. sleep function here halts the processing of the program for 1 second here
+'''
+    
+def deposit(username):
+    bal = int(input("\nEnter the amount to be deopited: $"))
+    balance[userArray.index(username)] += bal
+    sleep(1)
+    print("\nTRANSACTION SUCCESSFUL")
+    sleep(1)
+    printMainMenu(username)
+
+'''
+    Withdraw function:
+        1. first check if the user has 0 balance. if it is true then alerts the user of the 0 balance and tells them to deposit some money first
+        2. if it is false then asks the user to enter the amount to be withdrawn and checks wether it is greater than the balance of user or not
+        3. if it is greater than the balance then shows an error and goes to main menu and user has to again use the functionality
+        4. if it is smaller then the transaction is successful and user is directed to main menu after 1 second
+'''
+
+def withdraw(username):
+    if(balance[userArray.index(username)] == 0):
+        print("\nYou have 0 balance! Please deposit some money first.")
+        sleep(1)
+        printMainMenu(username)
+    else:
+        withMoney = int(input("\nEnter the amount to be withdrawn: $"))
+        if(withMoney > balance[userArray.index(username)]):
+                print("\nInsufficient balance in your account! Please try again!!")
+                sleep(1)
+                printMainMenu(username)
+        else:
+            balance[userArray.index(username)] -= withMoney
+            sleep(1)
+            print("\nTRANSACTION SUCCESSFUL")
+            sleep(1)
+            printMainMenu(username)
+
 '''
     Request balance function:
         1. displays the balacne of the user specified and then after 1 second the program exits to the main menu
